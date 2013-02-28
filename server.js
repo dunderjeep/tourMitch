53266654129bca05cbc8c03ce606a89d25f9a68b
unknown <8mc8@G248-062.caslab.queensu.ca>
2013-02-28 15:59:29 -0500

    'dbupd'

--- server.js
+++ server.js
@@ -45,11 +45,11 @@ app.get('/dpoi/:name/:descr/:latitude/:longitude?', api.save);
 app.get('/poi/:name.:format?', api.show);
 app.get('/poi', api.list);
 // set up the RESTful API, handler methods are defined in apiTour.js
-var api = require('./controller/apiTour.js');
-app.post('/tour', api.post);
-app.get('/tour/:name/:descr?', api.save);
-app.get('/tour/:name.:format?', api.show);
-app.get('/tour', api.list);
+var api2 = require('./controller/apiTour.js');
+app.post('/tour', apiTour.post);
+app.get('/tour/:name/:descr?', apiTour.save);
+app.get('/tour/:name.:format?', apiTour.show);
+app.get('/tour', apiTour.list);
 
 
 
