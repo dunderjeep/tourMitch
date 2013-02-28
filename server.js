9fddf914668936e868b28fc00acd6ee46ad2c09f
unknown <8mc8@queensu.ca>
2013-02-28 13:56:02 -0500

    'poi'

--- server.js
+++ server.js
@@ -39,11 +39,11 @@ app.configure(function () {
 
 // set up the RESTful API, handler methods are defined in api.js
 var api = require('./controller/api.js');
-app.post('/dogtag', api.post);
-app.get('/dogtag/:lon/:lat/:dist?', api.near);
-app.get('/dogtag/:name/:descr/:latitude/:longitude?', api.save);
-app.get('/dogtag/:name.:format?', api.show);
-app.get('/dogtag', api.list);
+app.post('/poi', api.post);
+app.get('/poi/:lon/:lat/:dist?', api.near);
+app.get('/dpoi/:name/:descr/:latitude/:longitude?', api.save);
+app.get('/poi/:name.:format?', api.show);
+app.get('/poi', api.list);
 
 
 //  And start the app on that interface (and port).
